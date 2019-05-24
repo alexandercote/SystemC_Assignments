40070165dd1afd726a6bae293c6f3be2186f90c8
Alexander Cote <alexcotework@gmail.com>
2019-05-23 21:31:56 -0700

    Added last part of Assignment 1 - folder 2

---
+++ 2 - Digital Filter as a Single Module/a2_main.cpp
@@ -0,0 +1,43 @@
+#include "systemc.h"
+#include "behavioural_digital_filter.h"
+#include <iostream>
+#include <iomanip>
+
+int sc_main(int argc, char* argv[])
+{
+    sc_signal <float> XSig;
+    sc_signal <float> YSig;
+    sc_signal <bool> RstSig;
+    sc_clock TestClk ("Testclock", 10, SC_NS, 0.5, 1, SC_NS);
+
+    a1_stimulus Stimulus1 ("Stimulator");
+    Stimulus1.x_input(XSig); 
+    Stimulus1.reset(RstSig);
+    Stimulus1.clock(TestClk);
+
+    a1_monitor Monitor1 ("MonitorTask");
+    Monitor1.x_input(XSig);
+    Monitor1.y_output(YSig);
+    Monitor1.clock(TestClk);
+
+    a2_digital_filter DF1 ("A2_Digital_Filter");
+    DF1.x_input(XSig);
+    DF1.y_output(YSig);
+    DF1.reset(RstSig);
+    DF1.clock(TestClk); 
+
+
+    sc_trace_file* Tf = sc_create_vcd_trace_file ("a2_digital_filter_traces");
+    Tf->set_time_unit(1, SC_NS); // Time unit is 1 ns
+    sc_trace(Tf, XSig, "X");
+    sc_trace(Tf, YSig, "Y");
+    sc_trace(Tf, RstSig, "RST");
+
+
+    sc_start (400, SC_NS);
+
+
+    sc_close_vcd_trace_file(Tf);
+
+    return 0;
+}
---
+++ 2 - Digital Filter as a Single Module/behavioural_digital_filter.h
@@ -0,0 +1,56 @@
+#include "systemc.h"
+
+SC_MODULE( a2_digital_filter )
+{
+    sc_in <float> x_input;
+    sc_in <bool> reset;
+    sc_in_clk clock;
+
+    sc_out <float> y_output;
+
+    // Register variables
+    sc_signal <float> zdelay_1_in, zdelay_2_in;
+    sc_signal <float> zdelay_1_out, zdelay_2_out;
+
+    void digital_filter()
+    {
+        zdelay_1_in.write( x_input.read() - ( 0.5*zdelay_1_out.read() + 0.25(zdelay_2_out.read() + x_input.read() ) ) );
+        zdelay_2_in.write( zdelay_1_out.read() );
+        y_output.write( zdelay_2_out.read() + ( 0.5*zdelay_1_out.read() + 0.25(zdelay_2_out.read() + x_input.read() ) ) );
+    }
+
+    void update_signals()
+    {
+        sc_signal<float> delay_store_1, delay_store_2;
+        delay_store_1.write(0);
+        delay_store_2.write(0);
+        zdelay_1_out.write(0);
+        zdelay_2_out.write(0);
+
+        while(true)
+        {
+            delay_store_1.write(zdelay_1_in.read());
+            delay_store_2.write(zdelay_2_in.read());
+
+            wait();
+
+            zdelay_1_out.write(delay_store_1.read());
+            zdelay_2_out.write(delay_store_2.read());
+        }
+    }
+
+
+    SC_CTOR (a2_digital_filter):
+        SC_METHOD(digital_filter);
+        sensitive << x_input << zdelay_1 << zdelay_2;
+        SC_CTHREAD(update_signals, clock.pos());
+        reset_signal_is(reset, true);
+
+    {
+ 
+
+
+
+
+    }
+};
