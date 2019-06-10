76b734276cd761740bc2dadd586de615c0e6b09c
Alex Cote <amcote@ugls23.ece.uvic.ca>
2019-06-10 11:19:20 -0700

    added new RDBLK function in memory, basic sc_main structure, testbench port init

--- A2Q1 - Memory Module/A2Q1_main.cpp
+++ A2Q1 - Memory Module/A2Q1_main.cpp
@@ -8,16 +8,15 @@ int sc_main(int argc, char* argv[])
 {
     // Memory module instance:
     mem_ctrl MC("MC");
-    
-    // Your testbench module(s) instances:
-    ...
-
+    // Testbench module instances:
+    A2Q1_testbench TB("TB");
 
     // Internal signals:
     sc_signal <bool> reset, new_comm, complete;
     sc_signal < sc_uint<2> > comm;
     sc_signal < sc_uint<8> > addr;
     sc_signal_rv <8> data;
+    sc_clock clk ("Testclock", 10, SC_NS, 0.5, 1, SC_NS);
     // _rv needed because of multiple drivers
   
 
@@ -30,7 +29,27 @@ int sc_main(int argc, char* argv[])
     MC.new_comm(new_comm);
     MC.complete(complete);
 
-    ...
+    TB.clk(clk);
+    TB.reset(reset);
+    TB.data(data)
+    TB.addr(addr);
+    TB.comm(comm);
+    TB.new_comm(new_comm);
+    TB.complete(complete);
+    
+    
+    sc_trace_file* Tf = sc_create_vcd_trace_file ("A2Q1_traces");
+    Tf->set_time_unit(1, SC_NS); // Time unit is 1 ns
+    sc_trace(Tf, data, "data");
+    sc_trace(Tf, addr, "addr");
+    sc_trace(Tf, comm, "comm");
+    sc_trace(Tf, new_comm, "new_comm");
+    sc_trace(Tf, complete, "complete");
+    sc_trace(Tf, reset, "reset");
+    
+    sc_start (202, SC_NS);
+
+    sc_close_vcd_trace_file(Tf);
 
 
     // Rest of sc_main ...
