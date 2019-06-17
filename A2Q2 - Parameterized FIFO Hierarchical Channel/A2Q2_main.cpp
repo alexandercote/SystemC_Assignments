18c5924341416ec0482d61663d37e3744df4235f
Alex Cote <alexcotework@gmail.com>
2019-06-17 12:12:48 -0700

    Update A2Q2_main.cpp

--- A2Q2 - Parameterized FIFO Hierarchical Channel/A2Q2_main.cpp
+++ A2Q2 - Parameterized FIFO Hierarchical Channel/A2Q2_main.cpp
@@ -10,7 +10,7 @@ int sc_main(int argc, char* argv[])
 	sc_clock ClkFast ("ClkFast", 5,  SC_NS, 0.5, 1, SC_NS);
 	sc_clock ClkSlow ("ClkSlow", 10, SC_NS, 0.5, 1, SC_NS);
 
-	fifo Fifo1 (”F1”, int, 8); // 8-integer FIFO
+	fifo<int,8> Fifo1 (”F1”); // 8-integer FIFO
 	producer P1 ("P1"); P1.out (Fifo1); P1.Clock (ClkFast);
 	consumer C1 ("C1"); C1.in (Fifo1);  C1.Clock (ClkSlow);
 	 
